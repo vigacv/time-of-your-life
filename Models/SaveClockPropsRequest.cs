222211334bc708a5b19faf6816debcfa9ab6beb1
Gabriel Carmelo Villaverde <vgabrielcv@gmail.com>
2024-05-16 21:26:42 -0500

    feat: added backend validation

--- Models/SaveClockPropsRequest.cs
+++ Models/SaveClockPropsRequest.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace time.Models;
 
 public record SaveClockPropsRequest(
+    [Required, MaxLength(50)]
     string Title,
+    [Required, MaxLength(50)]
     string FontFamily,
+    [Range(1, 100)]
     int TitleFontSize,
+    [Range(1, 100)]
     int ClockFontSize,
+    [Required, MaxLength(50)]
     string TitleFontColor,
+    [Required, MaxLength(50)]
     string ClockFontColor,
     bool BlinkColons
 );
